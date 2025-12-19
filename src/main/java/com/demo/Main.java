059b0704d279fc1dd8d53e1d2e0508a8951c8942
gustavosil591-boop <gustavosil591@gmail.com>
2025-12-19 18:17:25 +0000

    corrigindo

--- src/main/java/com/demo/Main.java
+++ src/main/java/com/demo/Main.java
@@ -12,12 +12,32 @@ public class Main {
 
     @RequestMapping("/")
     String home() {
-        return "Olá, seja bem vindo ao teste de Spring Boot para disciplina de Sistemas Distribuídos";
+        return "Olá, seja bem-vindo ao teste de Spring Boot para disciplina de Sistemas Distribuídos";
     }
 
     @RequestMapping("/home/{nome}")
     String piada(@PathVariable String nome) {
-        return "Olá "+ nome + ", você vai realmente conseguir passar nessa disciplina?";
+        return "Olá " + nome + ", você vai realmente conseguir passar nessa disciplina?";
+    }
+
+    @RequestMapping("/somar/{a}/{b}")
+    double somar(@PathVariable double a, @PathVariable double b) {
+        return a + b;
+    }
+
+    @RequestMapping("/subtrair/{a}/{b}")
+    double subtrair(@PathVariable double a, @PathVariable double b) {
+        return a - b;
+    }
+
+    @RequestMapping("/multiplicar/{a}/{b}")
+    double multiplicar(@PathVariable double a, @PathVariable double b) {
+        return a * b;
+    }
+
+    @RequestMapping("/dividir/{a}/{b}")
+    double dividir(@PathVariable double a, @PathVariable double b) {
+        return a / b;
     }
 
     public static void main(String[] args) {
